b9058a4fd8252471ddb3fed74c4fc6086e55fbe9
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-04-06 12:28:26 +0300

    feat(routes): register verify-email, forgot-password and reset-password endpoints to user router

--- job-portal-backend/routes/users.routes.js
+++ job-portal-backend/routes/users.routes.js
@@ -1,14 +1,18 @@
 
-import {Router } from "express";
+import { Router } from "express";
 import { protect } from '../middleware/auth.middleware.js';
-import { registerUser,loginUser,getCurrentUser } from "../controllers/users.controllers.js";
+import { registerUser, loginUser, getCurrentUser, verifyEmail, forgotPassword,resetPassword} from "../controllers/users.controllers.js";
 
 const router = Router();
 
-// end points
-router.route('/currentUser').get(protect, getCurrentUser);
+// Public routes
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/verify-email').get(verifyEmail);
+router.route('/forgot-password').post(forgotPassword);
+router.route('/reset-password').patch(resetPassword);
 
+// Protected routes
+router.route('/currentUser').get(protect, getCurrentUser);
 
 export default router;
