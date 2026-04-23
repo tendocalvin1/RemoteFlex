d76aaff94d7ac0bf4a2532cb8359e23051c17bd9
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-04-23 08:06:34 +0300

    feat(databse): switch from local MongoDB to Atlas cloud database

--- job-portal-backend/config/env.js
+++ job-portal-backend/config/env.js
@@ -6,3 +6,4 @@ export const EMAIL_PASS = process.env.EMAIL_PASS;
 export const EMAIL_FROM = process.env.EMAIL_FROM;
 export const CLIENT_URL = process.env.CLIENT_URL;
 export const JWT_SECRET = process.env.JWT_SECRET;
+export const MONGODB_URI = process.env.MONGODB_URI;
