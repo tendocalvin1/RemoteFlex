e19e86e72021135318f967b11234e2a8c9613e90
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-04-28 11:54:23 +0300

    feat(cors): update allowed origins for development environment and add socket test HTML

--- job-portal-backend/index.js
+++ job-portal-backend/index.js
@@ -14,7 +14,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: process.env.NODE_ENV === 'production'
       ? process.env.CLIENT_URL
-      : 'http://localhost:8000',
+      : ['http://localhost:3000', 'http://127.0.0.1:5500', 'http://localhost:5500'],
     credentials: true,
   },
 });
